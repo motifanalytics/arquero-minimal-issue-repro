6c7b77d3b423a65933c042a90fb0b2ac0bdac6bb
Danyel <danyel@motifanalytics.com>
2023-04-20 13:41:19 -0700

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'arquero-issue-repro-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'arquero-issue-repro';
+}
